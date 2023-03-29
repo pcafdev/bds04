a60142661df01643f54ac12542a42fd41b4a5703
Paulo Frota <paulo.frota@pcaf.dev.br>
2023-03-29 10:33:28 -0400

    Projeto com erro

---
+++ src/main/java/com/devsuperior/bds04/dto/UserInsertDTO.java
@@ -0,0 +1,53 @@
+package com.devsuperior.bds04.dto;
+
+import java.util.Objects;
+
+import com.devsuperior.bds04.services.validations.UserInsertValid;
+
+@UserInsertValid
+public class UserInsertDTO extends UserDTO{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String password;
+	
+	public UserInsertDTO() {
+		super();
+	}
+	
+
+	public UserInsertDTO(String password) {
+		super();
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(password);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserInsertDTO other = (UserInsertDTO) obj;
+		return Objects.equals(password, other.password);
+	}
+	
+	
+}
---
+++ src/main/java/com/devsuperior/bds04/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.bds04.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.bds04.entities.Role;
+
+//@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
